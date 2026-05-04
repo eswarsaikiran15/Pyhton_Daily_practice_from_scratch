1d7ff2584dcb20547fb796adf3a28f4f173af919
Kamparapu Eswar Sai Kiran <160008710+eswarsaikiran15@users.noreply.github.com>
2026-05-04 16:29:15 +0530

    Delete DSA_Practice_from_basic_to_advanced directory

--- DSA_Practice_from_basic_to_advanced/15_frequency_of_elements(naive).py
+++
@@ -1,9 +0,0 @@
-arr = [1,1, 2, 4, 3]
-
-for i in range(len(arr)):
-    count = 0
-    for j in range(len(arr)):
-        if arr[i] == arr[j]:
-            count += 1
-    print(arr[i], "→", count)
-
--- DSA_Practice_from_basic_to_advanced/19_missing_number.py
+++
@@ -1,6 +0,0 @@
-arr=[1,2,4,5,6,7,8,9]
-n=9
-expected=n*(n+1)//2
-actual=sum(arr)
-missing=expected-actual
-print("Missing number is:",missing)
