461df7e95acea6fc498cf41447dd677eff5b10ee
Kamparapu Eswar Sai Kiran <160008710+eswarsaikiran15@users.noreply.github.com>
2026-05-04 16:34:26 +0530

    Add files via upload

---
+++ DSA_Practice_from_basic_to_advanced/Arrays/9_sum_of_array.py
@@ -0,0 +1,9 @@
+def sum_of_array(arr: list) -> int:
+    total = 0
+    for num in arr:
+        total += num
+    return total
+
+arr=list(map(int, input("Enter numbers separated by space: ").split())) #taking input as a list of integers
+print("Sum of the array:", sum_of_array(arr))
+
